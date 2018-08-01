3d03089f83c287ee61d6e697c1b73951b5823fd3
Jun Jiang <smujiang@gmail.com>
2018-07-31 23:01:16 -0500

    change the file name to save

--- validate_exportation.py
+++ validate_exportation.py
@@ -14,13 +14,13 @@ im1 = Image.fromarray(I_patch)
 if im1.mode == "RGB":
     a_channel = Image.new('L', im1.size, 255)   # 'L' 8-bit pixels, black and white
     im1.putalpha(a_channel)
-im1.save("Img_patch.png")
+im1.save("Img_patch_val.png")
 
 mask = Image.open(mask_name)
 M = np.array(mask)
 M_patch = M[init_pos[0]:(init_pos[0]+patch_size[0]),init_pos[1]:(init_pos[1]+patch_size[1]),:]
 im2 = Image.fromarray(M_patch)
-im2.save("Mask_patch.png")
+im2.save("Mask_patch_val.png")
 blended= Image.blend(im1, im2, alpha=0.7)
 # blended.show()
 blended.save("Blended_patch.png")
